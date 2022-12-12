d4537784639b3a5c0182c6729367c52490ea3da1
ajaysingh8462 <ajaysingh84625@gmail.com>
2022-12-12 16:37:30 +0530

    [ajay]-using oops

--- line comperision/line comperision/Program.cs
+++ line comperision/line comperision/Program.cs
@@ -1,45 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Runtime.CompilerServices;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace line_comperision
 {
-    Console.WriteLine("Please enter x1 and y1 co-ordinates: ");
-    double x1 = Convert.ToDouble(Console.ReadLine());
-    double y1 = Convert.ToDouble(Console.ReadLine());
-    Console.WriteLine("please enter x2 and y2 co-ordinetes: ");
-    double x2 = Convert.ToDouble(Console.ReadLine());
-    double y2 = Convert.ToDouble(Console.ReadLine());
-
-    double distance = Math.Sqrt((Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)));
-        Console.WriteLine( "the length of to points is  "+(distance));
+    class program
     {
-        if ((x1 / x2 == y1 / y2))
-
-          Console.WriteLine("The given straight"
-        + " lines are equal");
-
-        else
-            Console.WriteLine("The given straight"
-            + " lines are not equal");
-
-       
-        
+        public static void Main(string[] args)
+        {
+            comparision line = new comparision();
+            line.LineComperision();
+        }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
---
+++ line comperision/line comperision/comparision.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace line_comperision
+{
+    internal class comparision
+    {
+        public void LineComperision()
+        {
+            Console.WriteLine("Please enter x1 and y1 co-ordinates: ");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("please enter x2 and y2 co-ordinetes: ");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            double y2 = Convert.ToDouble(Console.ReadLine());
+
+            double distance = Math.Sqrt((Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)));
+            Console.WriteLine("the length of to points is  " + (distance));
+            {
+                if ((x1 / x2 == y1 / y2))
+
+                    Console.WriteLine("The given straight"
+                  + " lines are equal");
+
+                else
+                    Console.WriteLine("The given straight"
+                    + " lines are not equal");
+                
+
+
+
+            }
+
+
+
+        }
+    }
+}
